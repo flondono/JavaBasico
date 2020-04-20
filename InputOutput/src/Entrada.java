4ec3ee6eae2a991830d3be01f99d0cc7125cd70c
flondono <flondono@bancolombia.com.co>
2020-04-20 10:50:02 -0500

    Se finaliza la clase entrada de datos

--- InputOutput/src/Entrada.java
+++ InputOutput/src/Entrada.java
@@ -17,6 +17,12 @@ public class Entrada {
         System.out.print("¿Qué edad tienes: ? ");
         edad = entrada.nextInt();
 
-        
+        System.out.print("¿Cuál es tu sueldo actual: ?");
+        sueldo = entrada.nextDouble();
+
+        System.out.println("----------------------------------------");
+        System.out.println("Tu nombes es: " + nombre);
+        System.out.println("Tu edad es: " + edad);
+        System.out.println("Tu sueldo actual es: " + sueldo);
     }
 }
