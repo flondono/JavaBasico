86930a882c651265b7e98521684fb666bc69629c
flondono <flondono89@gmail.com>
2020-04-27 18:24:04 -0500

    se finaliza de la clase envolventes

--- ClasesEnvolventes/src/wrapper/ClasesEnvolventes.java
+++ ClasesEnvolventes/src/wrapper/ClasesEnvolventes.java
@@ -6,14 +6,22 @@ public class ClasesEnvolventes {
 
         ClasesEnvolventes objeto = new ClasesEnvolventes();
         //objeto.xxxValue();
-        objeto.parseValueOf();
+        //objeto.parseValueOf();
+        objeto.metodoToString();
     }
 
+    public void metodoToString(){
+        Double d = new Double("1234");
+        String dCadena = d.toString();
+        System.out.println(dCadena);
+    }
     public void parseValueOf() {
         double d4 = Double.parseDouble("3.14");
         long primitivoLong = Long.parseLong("323");
+        Long objetoLong = Long.valueOf("456");
+        System.out.println(d4 + 1);
         System.out.println(primitivoLong + 1);
-        System.out.println(d4);
+        System.out.println(objetoLong + 1);
     }
 
     public void xxxValue() {
@@ -22,9 +30,9 @@ public class ClasesEnvolventes {
         short s = var.shortValue();
         double d = var.doubleValue();
 
-        System.out.println("El valor de var =" + var);
-        System.out.println("El valor de b =" + b);
-        System.out.println("El valor de s =" + s);
-        System.out.println("El valor de d =" + d);
+        System.out.println("El valor de var = " + var);
+        System.out.println("El valor de b = " + b);
+        System.out.println("El valor de s = " + s);
+        System.out.println("El valor de d = " + d);
     }
 }
